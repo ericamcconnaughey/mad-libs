caaf4b095400c18f3ef9715300b88d1ede7f5301
Erica McConnaughey <ericamcconnaughey@gmail.com>
2019-12-03 23:07:21 -0600

    add JS loops to clean up code

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,19 @@
 $(document).ready(function() {
   $("#blanks form").submit(function(event) {
+    var blanks = ["person1", "person2", "animal", "exclamation", "verb", "noun"];
+
+    blanks.forEach(function(blank) {
+      var userInput = $("input#" + blank).val();
+      $("." + blank).text(userInput;
+    })
+
+    $("#story").show();
+
+    event.preventDefault();
+  });
+});
+
+/* OLD CODE BEFORE LOOP 
     var person1Input = $("input#person1").val();
     var person2Input = $("input#person2").val();
     var animalInput = $("input#animal").val();
@@ -12,10 +26,4 @@ $(document).ready(function() {
     $(".animal").text(animalInput);
     $(".exclamation").text(exclamationInput);
     $(".verb").text(verbInput);
-    $(".noun").text(nounInput);
-
-    $("#story").show();
-
-    event.preventDefault();
-  });
-});
+    $(".noun").text(nounInput);*/
